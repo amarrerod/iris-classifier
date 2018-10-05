f1d79f000cde0a7aa3ed8bf3baae7f84d86581f8
marreA <alemarrerodiaz@gmail.com>
2018-10-05 19:00:35 +0100

    Comprobada la validacion cruzada

--- iris_classifier/classifier.py
+++ iris_classifier/classifier.py
@@ -4,13 +4,18 @@ import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection._validation import cross_val_score
 
 class KNN:
-    def __init__(self, dataset, x1 = None, x2 = None, y1 = None, y2 = None):
+    def __init__(self, dataset, x1 = None, x2 = None, y1 = None, y2 = None, cross_validation = True, folds = 0):
         self.data = dataset
         self.x = dataset.data
         self.y = dataset.target
-        self.split_data()
+        self.is_cross_validation = cross_validation
+        if cross_validation is not True:
+            self.split_data()
+        else:
+            self.folds = folds
         self.neighbors = 1
         self.model = KNeighborsClassifier(n_neighbors = self.neighbors)
     
@@ -31,3 +36,11 @@ class KNN:
         self.train(self.x1, self.y1)
         self.y2_model = self.model.predict(self.x2)
         return accuracy_score(self.y2, self.y2_model)
+
+    def cross_validation(self, folds = None):
+        if folds is None:
+            y2_model = self.model.fit(self.x1, self.y1).predict(self.x2)
+            y1_model = self.model.fit(self.x2, self.y2).predict(self.x1)
+            return [accuracy_score(self.y1, y1_model), accuracy_score(self.y2, y2_model)]
+        else:
+            return cross_val_score(self.model, self.x, self.y, cv = folds)
--- tests/test.py
+++ tests/test.py
@@ -5,6 +5,7 @@ from iris_classifier.classifier import KNN
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection._validation import cross_val_score
 
 class Test_Classifier(unittest.TestCase):
     def setUp(self):
@@ -13,7 +14,7 @@ class Test_Classifier(unittest.TestCase):
         self.y = self.iris.target
         self.x1, self.x2, self.y1, self.y2 = train_test_split(self.x, self.y, random_state = 0, train_size = 0.5)
         self.neighbors = 1
-        self.classifier = KNN(self.iris, self.x1, self.x2, self.y1, self.y2)
+        self.classifier = KNN(self.iris, self.x1, self.x2, self.y1, self.y2, cross_validation = False, folds = 1)
         self.classifier.train(self.x, self.y)
         self.classifier.predict(self.x)
 
@@ -50,3 +51,18 @@ class Test_Classifier(unittest.TestCase):
         y_model = self.classifier.model.predict(self.x2)
         expected = accuracy_score(self.y2, y_model)
         self.assertEqual(expected, self.classifier.holdout_validation())
+    
+    def test_cross_validation(self):
+        y2_model = self.classifier.model.fit(self.x1, self.y1).predict(self.x2)
+        y1_model = self.classifier.model.fit(self.x2, self.y2).predict(self.x1)
+        expected = [accuracy_score(self.y1, y1_model), accuracy_score(self.y2, y2_model)]
+        returned = self.classifier.cross_validation()
+        self.assertEqual(expected[0], returned[0])
+        self.assertEqual(expected[1], returned[1])
+
+    def test_cross_validation_5_folds(self):
+        model = KNeighborsClassifier(n_neighbors = self.neighbors)
+        expected = cross_val_score(model, self.x, self.y, cv = 5)
+        returned = self.classifier.cross_validation(5)
+        for i in range(5):
+            self.assertEqual(expected[i], returned[i])
