cb198d2d0739694df484b8e8131cf5440a756ab0
marreA <alemarrerodiaz@gmail.com>
2018-10-05 18:22:54 +0100

    Probando la validacion con conjuntos sin entrenar

--- iris_classifier/classifier.py
+++ iris_classifier/classifier.py
@@ -3,20 +3,31 @@
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 
 class KNN:
-    def __init__(self, dataset):
+    def __init__(self, dataset, x1 = None, x2 = None, y1 = None, y2 = None):
         self.data = dataset
         self.x = dataset.data
         self.y = dataset.target
+        self.split_data()
         self.neighbors = 1
         self.model = KNeighborsClassifier(n_neighbors = self.neighbors)
-        
-    def train(self):
-        self.model.fit(self.x, self.y)
+    
+    def split_data(self):
+        self.x1, self.x2, self.y1, self.y2 = train_test_split(self.x, self.y, random_state = 0, train_size = 0.5)
+    
+    def train(self, x = None, y = None):
+        self.model.fit(x, y)
     
     def predict(self, target):
         self.y_model = self.model.predict(target)
+        return self.y_model
         
     def score(self, target):
         return accuracy_score(target, self.y_model)
+    
+    def holdout_validation(self):
+        self.train(self.x1, self.y1)
+        self.y2_model = self.model.predict(self.x2)
+        return accuracy_score(self.y2, self.y2_model)
--- tests/test.py
+++ tests/test.py
@@ -3,15 +3,18 @@ import unittest
 from sklearn.datasets import load_iris
 from iris_classifier.classifier import KNN
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 class Test_Classifier(unittest.TestCase):
     def setUp(self):
         self.iris = load_iris()
-        self.classifier = KNN(self.iris)
         self.x = self.iris.data
         self.y = self.iris.target
+        self.x1, self.x2, self.y1, self.y2 = train_test_split(self.x, self.y, random_state = 0, train_size = 0.5)
         self.neighbors = 1
-        self.classifier.train()
+        self.classifier = KNN(self.iris, self.x1, self.x2, self.y1, self.y2)
+        self.classifier.train(self.x, self.y)
         self.classifier.predict(self.x)
 
     def test_shape_data(self):
@@ -35,3 +38,15 @@ class Test_Classifier(unittest.TestCase):
     def test_training_score_equals_one(self):
         value = self.classifier.score(self.classifier.y)
         self.assertAlmostEqual(value, 1.0)
+    
+    def test_has_two_sets(self):
+        self.assertEqual(self.x2.shape, self.classifier.x2.shape)
+
+    def test_has_holdout_validation(self):
+        self.assertTrue(self.classifier.holdout_validation())
+
+    def test_holdout_validation_equal(self):
+        self.classifier.train(self.x1, self.y1)
+        y_model = self.classifier.model.predict(self.x2)
+        expected = accuracy_score(self.y2, y_model)
+        self.assertEqual(expected, self.classifier.holdout_validation())
